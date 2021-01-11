8d6ef25e47d775997522afe69370e0b9c6aa6473
Colin Smith <colin.smith@wisc.edu>
2021-01-11 14:48:47 -0800

    Config for the routine handler R function

---
+++ config.R
@@ -0,0 +1,8 @@
+# Use this file to configure routine_handler.R
+
+config.repository <- "" # Repository system. Must be one of the supported repositories.
+config.environment <- "" # Repository environment. Some repositories have development and production environments.
+config.path <- "" # Path to directory containing workflow files
+config.www <- "" # Public URL for repository access to data and metadata files
+config.user.id <- "" # User ID for upload to repository
+config.user.pass <- "" # User password for upload to repository
---
+++ logs/README.md
@@ -0,0 +1,3 @@
+# /logs
+
+A directory for processing logs created by the routine_handler() function.
