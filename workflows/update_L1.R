16d63339474d759dc0b7e5719c2712e3342915c8
Colin Smith <colin.smith@wisc.edu>
2021-11-15 12:30:55 -0800

    Update dependencies

--- workflows/update_L1.R
+++ workflows/update_L1.R
@@ -128,45 +128,7 @@ download_and_source_conversion_script <- function(eml, path) {
     url = urls[script], 
     httr::write_disk(paste0(path, "/create_ecocomDP.R"), overwrite = TRUE), 
     httr::user_agent("ecocomDP"))
-  install_missing_libraries(
-    conversion.script = paste0(path, "/create_ecocomDP.R"))
   suppressMessages(source(paste0(path, "/create_ecocomDP.R")))
-}
-
-
-
-
-
-
-
-
-#' Install missing libraries
-#' 
-#' @description Identify if a create_ecocomDP.R script uses uninstalled libraries and install them for use in \code{update_L1()}.
-#' 
-#' @param conversion.script (character) Full path to create_ecocomDP.R
-#' 
-#' @details Libraries used by create_ecocomDP.R are identified by reading the full file to memory and using regular expressions to extract the name of the library from within \code{library()} (i.e. \code{"(?<=library\\().*(?=\\))"}). All libraries found in this way are installed.
-#'
-#' @export
-#'
-install_missing_libraries <- function(conversion.script) {
-  
-  lines <- readLines(conversion.script)
-  
-  libraries <- unlist(
-    stringr::str_extract_all(lines, "(?<=library\\().*(?=\\))"))
-  
-  installed_libraries <- row.names(installed.packages())
-  
-  if (!(all(libraries %in% installed_libraries))) {
-    missing_libraries <- libraries[which(!(libraries %in% installed_libraries))]
-    message(
-      "Installing missing libraries: ", 
-      paste(missing_libraries, collapse = ", "))
-    install.packages(missing_libraries)
-  }
-  
 }
 
 
