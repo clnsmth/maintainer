70a76fd9cba20e18fe9ece2cf94e85222d6d9977
Colin Smith <colin.smith@wisc.edu>
2021-11-16 20:11:19 -0800

    Allow access to workflow_manager()

--- webapp/caller.R
+++ webapp/caller.R
@@ -6,4 +6,4 @@ scripts <- list.files("/home/pasta/maintainer/workflows", full.names = TRUE)
 invisible(sapply(scripts, source))
 
 # Call the top level function that manages all workflows
-#workflow_manager()
+workflow_manager()
