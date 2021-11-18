b2e258352518cb1dc10bf4bd7ee1f96eec75782c
Colin Smith <colin.smith@wisc.edu>
2021-11-18 16:54:21 -0700

    Tester

---
+++ webapp/test.R
@@ -0,0 +1,26 @@
+#file.create("/home/pasta/maintainer/webapp/trigger_old.txt")
+#wd <- getwd()
+#libs <- rownames(installed.packages())
+#res <- c(wd, libs)
+get_from_queue <- function(filter = NULL) {
+  if (config.environment == "staging") {
+    url <- "https://regan.edirepository.org/maintainer/package-s.lternet.edu"
+  } else if (config.environment == "production") {
+    url <- "https://regan.edirepository.org/maintainer/package.lternet.edu"
+  }
+  if (!is.null(filter)) {
+    url <- paste0(url, "?filter=", filter)
+  }
+  resp <- httr::GET(url)
+  if (httr::status_code(resp) == 200) {
+    res <- readr::read_csv(
+      I(httr::content(resp, as = "text")), 
+      col_names = c("index", "id"), 
+      show_col_types = FALSE, )
+    return(res)
+  }
+}
+config.environment <- "staging"
+niq <- get_from_queue()
+res <- niq$id
+writeLines(text = res, con = "/home/pasta/maintainer/webapp/test_result.txt")
